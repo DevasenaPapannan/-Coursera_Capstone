1e4b9a95ca982348cef579ee8df4411816074742
DevasenaPapannan <67066704+DevasenaPapannan@users.noreply.github.com>
2020-08-23 21:59:39 +0530

    Add files via upload

---
+++ first one1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Applied Data Science Capstone
+
+#  Setting up Github Account for The Project
+# 
+
+# In[5]:
+
+
+get_ipython().system('pip install pandas')
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+

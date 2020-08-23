edf61fed72c533902af0722317b7d85832b952c5
DevasenaPapannan <67066704+DevasenaPapannan@users.noreply.github.com>
2020-08-23 21:52:18 +0530

    Add files via upload

---
+++ first one.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+get_ipython().system('pip install pandas')
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
